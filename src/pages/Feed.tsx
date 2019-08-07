d8f6b337f24233e950cd08e4748a19f82d6843b8
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2019-08-07 09:11:50 -0300

    Consultas e leituras do banco

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import api from '../services/api';
 
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, Text, FlatList, StyleSheet } from 'react-native';
 
 export default class Feed extends Component {
 	static navigationOptions = ({navigation}) => ({
@@ -14,7 +14,7 @@ export default class Feed extends Component {
 		feed: [],
 	};
 	async componentDidMount(){
-		//this.registerToSocket();
+		this.registerToSocket();
 
 		//exibir os dados para ver se esta certo
 		//console.log(response.data);
@@ -25,7 +25,110 @@ export default class Feed extends Component {
 		});
 
 	}
+
+	registerToSocket = () => {
+		const socket = io('http://localhost:3333');
+
+		socket.on('post', newPost => {
+			this.setState({ feed: [newPost, ...this.state.feed ]});
+		})
+		socket.on('like', likedPost => {
+			this.setState({
+				feed: this.state.feed.map(post => 
+					post._id === likedPost._id ? likedPost : post
+				)
+			});
+		})
+	}
+	handleLike = id => {
+		api.post(`/posts/${id}/like`);
+	}
 	render() {
-		return <View />;
+		return(
+			<View style={styles.container}>
+				<FlatList 
+					data={this.state.feed}
+					keyExtractor={post => post._id}
+					renderItem={({item}) => (
+						<View style={styles.feedItem}>
+							<View style={styles.userInfo}>
+								<Text style={styles.name}>{item.author}</Text>
+								<Text style={StyleSheet.place}>{item.place}</Text>
+							</View>
+							<Image source={more} />
+						</View>
+						<Image style={styles.feedImage} source={{ url: `http://localhost:3333/files/${item.image}`}} />
+						<View style={styles.feedItemFooter}>
+							<View style={styles.actions}>
+								<TouchableOpacity style={styles.action} onPress={() => this.handleLike(item._id)}>
+									<Image source={like} />
+								</TouchableOpacity>
+								<TouchableOpacity style={styles.action} onPress={() => {}}>
+									<Image source={comment} />
+								</TouchableOpacity>
+								<TouchableOpacity style={styles.action} onPress={() => {}}>
+									<Image source={send} />
+								</TouchableOpacity>
+							</View>
+							<Text style={styles.likes}>{item.likes} curtidas</Text>
+							<Text style={styles.description}>{item.description}</Text>
+							<Text style={styles.hashtags}>{item.hashtags}</Text>
+						</View>
+
+					)}
+				/>
+			</View>
+
+		);
 	}
 }
+const styles = StyleSheet.create({
+	container:{
+		flex: 1
+	},
+	feedItem:{
+		marginTop: 20
+	},
+	feedItemHeader:{
+		paddingHorizontal: 15,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+	},
+	name:{
+		fontSize: 14,
+		color: '#000'
+	},
+	place:{
+		fontSize: 12,
+		color: '#666',
+		marginTop: 2
+	},
+	feedImage:{
+		width: "100%",
+		heigth: 400,
+		marginVertical: 15
+	},
+	feedItemFooter:{
+		paddingHorizontal: 15
+	},
+	actions: {
+		flexDirection: 'row'
+	},
+	action: {
+		marginRight: 8
+	},
+	likes:{
+		marginTop: 15,
+		fontWeight: 'bold',
+		color: '#000'
+	},
+	description:{
+		lineHeight: 18,
+		color: '#000'
+	},
+	hashtags:{
+		color: '#7159C1'
+	}
+
+});
--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, TextInput } from 'react-native';
 
 export default class New extends Component {
-  render() {
-	return <View />;
-  }
+	static navigationOptions = {
+		headerTitle: 'Nova Publicacao'
+	};
+	state = {
+		author: '',
+		place: '',
+		description: '',
+		hashtags: '',
+	};
+  	render() {
+		return(
+			<View style={styles.container} >
+				<TouchableOpacity style={styles.selectButton} onPress={() => {}}>
+					<Text style={styles.selectButton}>Selecionar Imagens</Text>
+				</TouchableOpacity>
+				<TextInput 
+					style={styles.input}
+					autoCorrect={false}
+					autoCapitalize="none"
+					placeholder="Nome do autor"
+					placeholderTextColor="#999"
+					value={this.state.author}
+					onChangeText={author => this.setState({ author })}
+				/>
+			</View>
+		);
+  	}
 }
+const styles = StyleSheet.create({
+
+});
